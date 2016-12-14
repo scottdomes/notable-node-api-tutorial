b233d02b0215b9ec778d7ff5c44e26a7e9692a2c
scottdomes <scottdomes@gmail.com>
2016-12-13 20:51:36 -0800

    Added database

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,7 @@
 module.exports = function(app, db) {
-
-
+  app.post('/notes', (req, res) => {
+    console.log(req.body)
+    // We'll create our note here.
+    res.send('Hello')
+  });
 };
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+const db             = require('./config/db');
 
 const app            = express();
 
 const port = 8000;
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});    
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('Magic happens on port ' + port);
+  });               
+}) 
