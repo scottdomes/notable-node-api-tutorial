7a0ae6545266fa800079eeea4ca0a08add92a761
scottdomes <scottdomes@gmail.com>
2016-12-13 20:23:40 -0800

    Set up routes

---
+++ app/routes/index.js
@@ -0,0 +1,5 @@
+const noteRoutes = require('./note_routes');
+
+module.exports = function(app, db) {
+  noteRoutes(app, db);
+};
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app            = express();
 
 const port = 8000;
 
+require('./app/routes')(app, {});
 app.listen(port, () => {
   console.log('We are live on ' + port);
 });    
